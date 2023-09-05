9c6d7f67dd4daaf921dee88d3c2ab2f60df11088
Gustavolmg <gustavolmgomes@gmail.com>
2023-09-05 15:21:35 -0300

    add index

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css\bootstrap-icons-1.10.5\font\bootstrap-icons.min.css">
+	<link rel="shortcut icon" href="css/bootstrap-icons-1.10.5/house-fill.svg" type="image/x-icon">
+</head>
+<body>
+
+     <div class="container-fluid">
+          <div class="row position-relative" style="height: 100vh;">
+
+               <div class="col-1 bg-primary bg-opacity-50" >
+
+                         <div class="nav flex-column text-center text-light-emphasis">
+                              <div class="nav-item pt-2 h1">SBE</div>
+                              <div class="nav-item pt-3 h2"><i class="bi bi-house-fill"></i></div>
+                              <div class="nav-item pt-3 h2"><i class="bi bi-person-lines-fill"></i></div>
+                              <div class="nav-item pt-3 h2"><i class="bi bi-people-fill"></i></div>
+                              <div class="nav-item pt-3 h2"><i class="bi bi-cart-fill"></i></div>
+                              <div class="nav-item pt-3 h2"><i class="bi bi-truck-front-fill"></i></div>
+                              <div class="nav-item pt-3 h2"><i class="bi bi-clipboard2-fill"></i></div>
+                              <div class="nav-item pt-3 h2"><i class="bi bi-gear-fill"></i></div>
+
+                         </div>
+                         
+               </div>
+
+               <div class="col-11 bg-danger">
+                         sdg
+               </div> 
+
+          </div>
+     </div>
+     
+</body>
+
+     <script type="text/javascript" src="Js/bootstrap.bundle.min.js"></script>
+
+</html>

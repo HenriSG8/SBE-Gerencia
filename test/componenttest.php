c0eb44b709fe544405a27b51ad2a4152c508716b
Gustavolmg <gustavolmgomes@gmail.com>
2023-09-08 12:27:44 -0300

    add layout

---
+++ test/componenttest.php
@@ -0,0 +1,11 @@
+<div class="row rounded-bottom-3 text-center h3 py-3 bg-white">
+     <div class="col-4 border-end-2">
+          Produtos
+     </div>
+     <div class="col-4">
+          Gastos
+     </div>
+     <div class="col-4">
+          Materias
+     </div>
+</div>
--- test/indextest.php
+++ test/indextest.php
@@ -10,9 +10,12 @@
 </head>
 <body>
 
+     <!-- container -->
      <div class="container-fluid">
+          <!-- main row -->
           <div class="row position-relative" style="height: 100vh;">
 
+               <!-- Side navbar -->
                <div class="col-1 bg-primary bg-opacity-50" >
 
                          <div class="nav flex-column text-center text-light-emphasis">
@@ -23,21 +26,151 @@
                               <div class="nav-item pt-3 h2"><i class="bi bi-cart-fill"></i></div>
                               <div class="nav-item pt-3 h2"><i class="bi bi-truck-front-fill"></i></div>
                               <div class="nav-item pt-3 h2"><i class="bi bi-clipboard2-fill"></i></div>
+                              <div class="nav-item pt-3 h2"><i class="bi bi-search"></i></div>
                               <div class="nav-item pt-3 h2"><i class="bi bi-gear-fill"></i></div>
 
                          </div>
-                         
+               <!-- end Side navbar -->
                </div>
 
+               <!-- Main content -->
                <div class="col-11 bg-danger">
-                         sdg
-               </div> 
 
+                    <!-- Products, Expenses and materials navbar -->
+                    <div class="row text-center fs-2 fw-semibold py-3 bg-light">
+                         <div class="col-4 border-end">
+                              Produtos
+                         </div>
+                         <div class="col-4 border-end">
+                              Gastos
+                         </div>
+                         <div class="col-4">
+                              Materias
+                         </div>
+                         
+                    <!-- end Products, Expenses and materials navbar-->
+                    </div>
+                    
+                    <!-- Products window -->
+                    <div class="row text-center bg-white py-2" >
+
+                         <!-- Product´s Register window -->
+                         <div class="col-4 py-1 border border-opacity-50">
+                            <span class=" h3">
+                              Cadastrar
+                            </span>
+                            <div>
+                              <form action="">
+                                   <div class="my-2 input-group">
+                                        <div class="input-group-text" >Nome</div>
+                                        <input type="text" class="form-control">
+                                   </div>
+                                   <div class="my-2 input-group">
+                                        <div class="input-group-text" >Descrição</div>
+                                        <textarea class="form-control"> </textarea>
+                                   </div>
+                                   <div class="my-2 input-group">
+                                        <div class="input-group-text" >Valor</div>
+                                        <input type="text" class="form-control">
+                                   </div>
+                                   <div class="my-2 input-group">
+                                        <div class="input-group-text" >Material</div>
+                                        <select class="form-select" name="" id="">
+                                             <option value="">aluminio</option>
+                                             <option value="">short do hulk</option>
+                                             <option value="">ferro</option>
+                                        </select>
+                                   </div>
+                                   <div class="my-2">
+                                        <div class="d-grid my-1"><button type="submit" class="btn btn-warning">Limpar</button></div>
+                                        <div class="d-grid my-1"><button type="submit" class="btn btn-success">Cadastrar</button></div>                                        
+                                   </div>
+                              </form>
+                            </div>
+                         <!-- end Product´s Register window-->
+                         </div>
+
+                         <!-- Product´s Change window -->
+                         <div class="col-4 py-1 border border-opacity-50">
+                            <span class=" h3">
+                              Alterar
+                            </span>
+                            <div>
+                              <form action="">
+                                   <div class="my-2 input-group">
+                                        <div class="input-group-text" >Nome</div>
+                                        <input type="text" class="form-control">
+                                   </div>
+                                   <div class="my-2 input-group">
+                                        <div class="input-group-text" >Descrição</div>
+                                        <textarea class="form-control"> </textarea>
+                                   </div>
+                                   <div class="my-2 input-group">
+                                        <div class="input-group-text" >Valor</div>
+                                        <input type="text" class="form-control">
+                                   </div>
+                                   <div class="my-2 input-group">
+                                        <div class="input-group-text" >Material</div>
+                                        <select class="form-select" name="" id="">
+                                             <option value="">aluminio</option>
+                                             <option value="">short do hulk</option>
+                                             <option value="">ferro</option>
+                                        </select>
+                                   </div>
+                                   <div class="my-2">
+                                        <div class="d-grid my-1"><button type="submit" class="btn btn-warning">Limpar</button></div>
+                                        <div class="d-grid my-1"><button type="submit" class="btn btn-success">Alterar</button></div>                                        
+                                   </div>
+                              </form>
+                            </div>
+                         <!-- end Product´s Chance window-->
+                         </div>
+
+                         <!-- Product´s Delete window -->
+                         <div class="col-4 py-1 border border border-opacity-50">
+                              <span class=" h3">
+                              Excluir
+                              </span>
+                              <div>
+                                   <form action="">
+                                        <div class="my-2 input-group">
+                                             <span class="input-group-text">Nome Produto</span>
+                                             <select class="form-select" name="" id="">
+                                                  <option value="">Porta</option>
+                                                  <option value="">Grade</option>
+                                                  <option value="">calcinha da She-hulk</option>
+                                                  <option value="">Janelas</option>
+                                             </select>
+                                        </div>
+                                        <div class="my-2 form-check text-start">
+                                             <input type="checkbox" class="form-check-input" for="product_del_ch" name="" id="" required>
+                                             <label class="form-check-label" id="product_del_ch">
+                                                  Tem certeza?
+                                             </label>
+                                        </div>
+                                        <div class="my-2">
+                                             <div class="d-grid my-1"><button type="submit" class="btn btn-danger">Deletar</button></div>                                        
+                                        </div>
+                                   </form>
+                              </div>
+                         <!-- end Product´s Delete window-->
+                         </div>
+
+                    <!-- end Product window -->
+                    </div>
+
+
+               <!-- end main contant -->
+               </div> 
+          
+          <!-- end main row -->
           </div>
+
+     <!-- end container -->
      </div>
      
 </body>
 
-     <script type="text/javascript" src="Js/bootstrap.bundle.min.js"></script>
+     <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
 
 </html>
